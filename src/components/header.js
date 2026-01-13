b014075c6b8883db1f12f8142b31d2d4d14aa11c
HydroX04 <dynartubigan.1@gmail.com>
2026-01-13 12:26:28 +0800

    header

--- src/components/header.js
+++ src/components/header.js
@@ -208,7 +208,8 @@ export default function AppHeader() {
 
       // Create WebSocket connection with token auth
       const wsProtocol = window.location.protocol === "https:" ? "wss" : "ws";
-      ws = new WebSocket(`${wsProtocol}://notification-service-vbs9.onrender.com/ws/notifications/${username}?token=${token}`
+      const encodedToken = encodeURIComponent(token);
+      ws = new WebSocket(`${wsProtocol}://notification-service-vbs9.onrender.com/ws/notifications/${username}?token=${encodedToken}`
       );
 
       ws.onopen = () => {
